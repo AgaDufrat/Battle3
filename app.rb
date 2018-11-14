5640e9ba9c17753de3a26c7074cf245983b9c166
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-14 14:22:19 +0000

    refactored using helper method

--- app.rb
+++ app.rb
@@ -6,17 +6,17 @@ class Battle < Sinatra::Base
     erb(:player_form)
   end
 
-  get '/play' do
-    @player_1 = session[:player_1]
-    @player_2 = session[:player_2]
-    erb(:player_names)
-  end
-
   post '/names' do
     p params
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
     redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:player_names)
   end
   run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,10 +1,7 @@
 feature 'Testing infrastructure' do
 
   scenario 'Can allow players to enter their name and see their HP' do
-    visit('/')
-    fill_in 'player_1', with: 'Bob'
-    fill_in 'player_2', with: 'James'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content 'Bob HP is 100 and James HP is 100'
   end
 end
